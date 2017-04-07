447e54485f2933e16afb48887934969bd0cb14be
aglorioso <alexandra.glorioso@naplesnews.com>
2017-04-07 09:32:02 -0400

    Froze my app

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def detail(row_id):
     for row in object_list:
         if row['row_id'] == row_id:
             return render_template(template, object=row)
-        abort(404)
+    abort(404)
 
 if __name__ == '__main__':
     # Fire up the Flask test server
